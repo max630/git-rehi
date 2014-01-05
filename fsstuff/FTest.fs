86b88bdd15722e81d17d01a9ae9973b7a37cb27c
Max Kirillov <max@max630.net>
2014-01-06 01:55:45 +0200

    IMPL: resolveHashes finally

--- fsstuff/FTest.fs
+++ fsstuff/FTest.fs
@@ -1,5 +1,19 @@
 module rebase2.FTest
 
+let intersperse sep sq =
+    let fun1 st it = match st with
+                     | (true, acc) -> (true, acc ^ sep ^ it)
+                     | (false, acc) -> (false, acc ^ it)
+    let (_, res) = Seq.fold fun1 (false, "") sq
+    res
+
 let resolveHashes refs =
-    for ref in refs do
+    let refs_list = Seq.toList refs
+    for ref in refs_list do
         IOUtils.verifyCmdArg ref
+    let refs_str = intersperse " " refs_list
+    (* newlines should be stripped by EnumPopen implementation *)
+    let res = Seq.toList (IOUtils.EnumPopen("git", "rev-parse " ^ refs_str))
+    if (List.length res <> List.length refs_list) then
+        raise (System.Exception "Hash number does not match")
+    res
