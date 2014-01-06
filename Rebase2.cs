57d2c4e37674448595ceffec6d52e63434392f88
Max Kirillov <max@max630.net>
2014-01-06 19:57:59 +0200

    CC: formatting change

--- Rebase2.cs
+++ Rebase2.cs
@@ -23,34 +23,34 @@ public class Rebase2 {
             if (Regex.IsMatch(todoLine, @"^\s*#"))
                 continue;
             switch (mode) {
-                case Mode.COMMAND:
-                    if ((match = Regex.Match(todoLine, @"^(f|fixup) ([0-9a-f]+)( (.*))?$")).Success) {
-                        Todo.Add(Types.Step.NewFixup(match.Groups[2].Value));
-                        if (!commits.byAHash.ContainsKey(match.Groups[2].Value))
-                            unknownCommits.Add(match.Groups[2].Value);
-                    } else if ((match = Regex.Match(todoLine, @"^(p|pick) ([0-9a-f]+)( (.*))?$")).Success) {
-                        Todo.Add(Types.Step.NewPick(match.Groups[2].Value));
-                        if (!commits.byAHash.ContainsKey(match.Groups[2].Value))
-                            unknownCommits.Add(match.Groups[2].Value);
+            case Mode.COMMAND:
+                if ((match = Regex.Match(todoLine, @"^(f|fixup) ([0-9a-f]+)( (.*))?$")).Success) {
+                    Todo.Add(Types.Step.NewFixup(match.Groups [2].Value));
+                    if (!commits.byAHash.ContainsKey(match.Groups [2].Value))
+                        unknownCommits.Add(match.Groups [2].Value);
+                } else if ((match = Regex.Match(todoLine, @"^(p|pick) ([0-9a-f]+)( (.*))?$")).Success) {
+                        Todo.Add(Types.Step.NewPick(match.Groups [2].Value));
+                        if (!commits.byAHash.ContainsKey(match.Groups [2].Value))
+                            unknownCommits.Add(match.Groups [2].Value);
                     } else if ((match = Regex.Match(todoLine, @"^(e|edit) ([0-9a-f]+)( (.*))?$")).Success) {
-                        Todo.Add(Types.Step.NewEdit(match.Groups[2].Value));
-                        if (!commits.byAHash.ContainsKey(match.Groups[2].Value))
-                            unknownCommits.Add(match.Groups[2].Value);
-                    } else if ((match = Regex.Match(todoLine, @"^(x|exec) (.+)$")).Success) {
-                        Todo.Add(Types.Step.NewExec(match.Groups[2].Value));
-                    } else if ((match = Regex.Match(todoLine, @"^comment\b$")).Success) {
-                        mode = Mode.COMMENT;
-                        comment = "";
-                    } else if ((match = Regex.Match(todoLine, @"^merge( --ours)? (.+[^\n\r ])$")).Success) {
-                        mode = Mode.MERGE_COMMENT;
-                        comment = "";
-                        merge_parents = new List<string>(Regex.Split(match.Groups[2].Value, @" +"));
-                    }
-                    break;
-                case Mode.COMMENT:
-                    break;
-                case Mode.MERGE_COMMENT:
-                    break;
+                            Todo.Add(Types.Step.NewEdit(match.Groups [2].Value));
+                            if (!commits.byAHash.ContainsKey(match.Groups [2].Value))
+                                unknownCommits.Add(match.Groups [2].Value);
+                        } else if ((match = Regex.Match(todoLine, @"^(x|exec) (.+)$")).Success) {
+                                Todo.Add(Types.Step.NewExec(match.Groups [2].Value));
+                            } else if ((match = Regex.Match(todoLine, @"^comment\b$")).Success) {
+                                    mode = Mode.COMMENT;
+                                    comment = "";
+                                } else if ((match = Regex.Match(todoLine, @"^merge( --ours)? (.+[^\n\r ])$")).Success) {
+                                        mode = Mode.MERGE_COMMENT;
+                                        comment = "";
+                                        merge_parents = new List<string>(Regex.Split(match.Groups [2].Value, @" +"));
+                                    }
+                break;
+            case Mode.COMMENT:
+                break;
+            case Mode.MERGE_COMMENT:
+                break;
             }
         }
     }
