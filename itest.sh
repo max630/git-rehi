816d1e3848b78943663fac801ec15f5ec0729ac2
Max Kirillov <max@max630.net>
2014-01-12 02:07:26 +0200

    TEST: a bit more visible output

--- itest.sh
+++ itest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO: set up some trap to indicate that the error happened
 set -e
 
 # git clone ../rebase2-test-repo /tmp/git-rebase/test-repo
@@ -15,3 +16,5 @@ reset_repo() {
 git reset --hard origin/b2
 "$DIR/git-rebase2" origin/b1
 git diff --quiet origin/master
+
+echo ALL PASSED
