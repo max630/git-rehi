6dec016e9613252ed8f28638f954d3dd7e84412d
Max Kirillov <max@max630.net>
2014-01-12 14:12:38 +0200

    TMP: fixes for git-1.7

--- itest.sh
+++ itest.sh
@@ -8,11 +8,14 @@ DIR=`pwd`
 cd /tmp/git-rebase/test-repo
 
 reset_repo() {
-    git checkout -f --no-track -B master origin/master
+    "$DIR/git-rebase2" --abort || :
+    git checkout -f master
+    git reset --hard origin/master
     git clean -f -x -d
 }
 
 # SMOKE
+reset_repo
 git reset --hard origin/b2
 "$DIR/git-rebase2" origin/b1
 git diff --quiet origin/master
