0477c6b609ca58ff3f3288d44ef76979e18422c9
Max Kirillov <max@max630.net>
2014-04-12 01:10:28 +0300

    gh-19: add test for parsin branches

--- test.pl
+++ test.pl
@@ -85,6 +85,9 @@ isnt (do { eval { parse_cli(['a', 'b....d', 'c']) }; $@ }, '');
 is_deeply (read_todo(\<<End, []), [{ type => "pick", ahash => "12345"}]);
 pick 12345
 End
+is_deeply (read_todo(\<<End, []), [{ type => "pick", ahash => "origin/master"}]);
+pick origin/master
+End
 is_deeply (read_todo(\<<End, []), [{ type => "merge", parents => ["HEAD", "12345"], flags => {}, ahash => "93845345"}]);
 merge -c 93845345 HEAD,12345 Test merge comment
 End
