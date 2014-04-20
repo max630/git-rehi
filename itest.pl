ed4d084c49a044fb985d6385777166d8048058f1
Max Kirillov <max@max630.net>
2014-04-21 00:21:34 +0300

    TEST: this one is done now

--- itest.pl
+++ itest.pl
@@ -167,7 +167,6 @@ t {
 } inner_merge_detected;
 
 t {
-    local $TODO = "gh-19";
     my $g = env_guard->new("GIT_SEQUENCE_EDITOR", "$SOURCE_DIR/itest-edit.sh");
     my $gc = env_guard->new("GIT_SEQUENCE_EDITOR_CASE", "merge-inner");
     cmd("$testee -i origin/base~1");
