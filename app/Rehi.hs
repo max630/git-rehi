eca3111406206392e0cebe8d00daa6c43defaa90
Max Kirillov <max@max630.net>
2015-12-14 00:59:07 +0200

    PARTIAL: rewriting

---
+++ app/Rehi.hs
@@ -0,0 +1,164 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+module Rehi where
+
+import Prelude hiding (putStrLn)
+
+import Data.ByteString(ByteString,putStrLn)
+import Data.Maybe(fromMaybe)
+import Control.Monad(liftM)
+import Control.Monad.Trans.Reader(ReaderT(runReaderT),ask)
+import Control.Monad.Trans.Class(lift)
+import System.Posix.ByteString(RawFilePath,removeLink,fileExist)
+import System.Posix.Env.ByteString(getArgs)
+
+import qualified Data.Map.Strict as Map
+
+main :: IO _
+main = do
+  env <- get_env
+  flip runReaderT env $ do
+    args <- lift getArgs
+    let parsed = parse_cli args
+    case parsed of
+      Abort -> abort_rebase
+      Continue -> do
+        (todo, current, commits, target_ref) <- restore_rebase
+        case current of
+          Just c -> do
+            run_continue c commits
+            lift (removeLink (envGitDir env `mappend` "/rehi/current"))
+          Nothing -> return ()
+        let commits' = commits { stateHead = Sync }
+        run_rebase todo commits' target_ref
+      Skip -> do
+        (todo, current, commits, target_ref) <- restore_rebase
+        case current of
+          Just c -> do
+            lift (run_command "git rest --hard HEAD")
+            lift (removeLink (envGitDir env `mappend` "/rehi/current"))
+      Current -> do
+        let currentPath = envGitDir env `mappend` "/rehi/current"
+        lift (fileExist currentPath) >>= \case
+          True -> do
+            content <- lift $ read_file currentPath
+            lift $ putStrLn ("Current: " `mappend` content)
+          False -> error "No rehi in progress"
+      Run dest source_from_arg through source_to_arg target_arg interactive -> do
+        git_verify_clean
+        initial_branch <- git_get_checkedout_branch
+        let
+          target_ref = fromMaybe initial_branch target_arg
+          source_to = fromMaybe target_ref source_to_arg
+        source_from <- case source_from_arg of
+          Just s -> pure s
+          Nothing | Just _ <- regex_match dest ".*~1$" -> pure dest
+          Nothing -> git_merge_base source_to dest
+        let
+          through' = case regex_match source_from "^(.*)~1$" of
+            Just (_ : m : _) -> m : through
+            Nothing -> through
+        main_run dest source_from through' source_to target_ref initial_branch interactive
+
+data CliMode =
+  Abort
+  | Continue
+  | Skip
+  | Current
+  | Run ByteString (Maybe ByteString) [ByteString] (Maybe ByteString) (Maybe ByteString) Bool
+
+newtype Hash = Hash ByteString deriving (Eq, Ord, Show)
+
+data Head = Sync | Known Hash
+
+data Commits = Commits {
+    stateHead :: Head
+  , stateRefs :: Map.Map ByteString Hash
+  , stateMarks :: Map.Map ByteString Hash
+  , stateByHash :: Map.Map Hash Entry
+  }
+
+data Entry = Entry {
+    entryAHash :: ByteString
+  , entryHash :: Hash
+  , entrySubject :: ByteString
+  , entryParents :: [Hash]
+  , entryTree :: Hash
+  }
+
+data Step =
+    Pick Hash
+  | Fixup Hash
+  | Edit Hash
+  | Exec ByteString
+  | Comment ByteString
+  | Merge (Maybe ByteString) [ByteString] Bool Bool
+  | Mark ByteString
+  | Reset ByteString
+  | UserComment ByteString
+  | TailPickWithComment ByteString ByteString
+
+data Env = Env { envGitDir :: RawFilePath }
+
+parse_cli :: [ByteString] -> CliMode
+parse_cli = parse_loop False
+  where
+    parse_loop _ ("-i" : argv') = parse_loop True argv'
+    parse_loop _ ("--interactive" : argv') = parse_loop True argv'
+    parse_loop _ argv@("--abort" : _ : _ ) = error ("Extra argument:" ++ show argv)
+    parse_loop _ ["--abort"] = Abort
+    parse_loop _ argv@("--continue" : _ : _ ) = error ("Extra argument:" ++ show argv)
+    parse_loop _ ["--continue"] = Continue
+    parse_loop _ argv@("--skip" : _ : _ ) = error ("Extra argument:" ++ show argv)
+    parse_loop _ ["--skip"] = Skip
+    parse_loop _ argv@("--current" : _ : _ ) = error ("Extra argument:" ++ show argv)
+    parse_loop _ ["--current"] = Current
+    parse_loop interactive [dest] = Run dest Nothing [] Nothing Nothing interactive
+    parse_loop interactive (arg0 : arg1 : arg2mb) | length arg2mb == 1 || length arg2mb == 0 && Just _ <- regex_match arg1 "\\.\\." = 
+        let
+          re_ref0 = "(?:[^\\.]|(?<!\\.)\\.)*"
+          re_ref1 = "(?:[^\\.]|(?<!\\.)\\.)+"
+          re_sep = "(?<!\\.)\\.\\."
+          (source_from, through, source_to) = case regex_match arg1 (mconcat ["(", re_ref0, ")", re_sep, "((?:", re_ref1, re_sep, ")*)(", re_ref0, ")^$"]) of
+            Just [all, m1, m2, m3] -> (m1, regex_match_all m2 (mconcat ["(", re_ref1, ")", re_sep]), m3)
+            _ -> error ("Invalid source spec:" ++ show arg1)
+          arg2 = case arg2mb of
+            [] -> Nothing
+            [v] -> Just v
+        in Run arg0 (Just source_from) through (Just source_to) arg2 interactive
+    parse_loop interactive [arg0, arg1] = Run arg0 Nothing [] Nothing (Just arg1) interactive
+    parse_loop _ argv = error ("Invalid arguments: " ++ show argv)
+  
+
+main_run = undefined
+
+get_env = undefined
+
+abort_rebase = undefined
+
+restore_rebase = undefined
+
+run_continue = undefined
+
+read_file = undefined
+
+git_verify_clean = undefined
+
+git_get_checkedout_branch = undefined
+
+git_merge_base = undefined
+
+regex_match :: ByteString -> ByteString -> Maybe [ByteString]
+regex_match = undefined
+
+regex_match_all = undefined
+
+run_rebase = undefined
+
+run_command = undefined
+
+listenGitDir :: Monad m => ReaderT Env m RawFilePath
+listenGitDir = ask >>= \r -> pure (envGitDir r)
