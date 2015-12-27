27c642d4ec3a135f9bd2a1e35bc10a7a3951f22c
Max Kirillov <max@max630.net>
2015-12-27 18:53:35 +0200

    no direct posix dependency

--- app/System/File/ByteString.hs
+++ app/System/File/ByteString.hs
@@ -1,6 +1,8 @@
 module System.File.ByteString (
     readFile,
-    withFile
+    withFile,
+    openFile,
+    openBinaryTempFile
   ) where
 
 import Prelude hiding (readFile)
@@ -8,8 +10,8 @@ import Prelude hiding (readFile)
 import Control.Exception (throwIO)
 import Control.Monad (join)
 import Data.ByteString (ByteString)
-import Data.Text (unpack)
-import Data.Text.Encoding (decodeUtf8')
+import Data.Text (unpack,pack)
+import Data.Text.Encoding (decodeUtf8',encodeUtf8)
 
 import qualified Data.ByteString as B
 import qualified System.IO as SI
@@ -18,6 +20,16 @@ withFile :: ByteString -> SI.IOMode -> (SI.Handle -> IO ()) -> IO ()
 withFile p m f = join $ SI.withFile <$> decode p <*> pure m <*> pure f
 
 readFile p = decode p >>= B.readFile
+
+openFile :: ByteString -> SI.IOMode -> IO SI.Handle
+openFile p m = join $ SI.openFile <$> decode p <*> pure m
+
+openBinaryTempFile :: ByteString -> ByteString -> IO (ByteString, SI.Handle)
+openBinaryTempFile dir fn = do
+  dir <- decode dir
+  fn <- decode fn
+  (p, h) <- SI.openBinaryTempFile dir fn
+  pure (encodeUtf8 $ pack p, h)
 
 decode :: ByteString -> IO String
 decode = either throwIO (pure . unpack) . decodeUtf8'
