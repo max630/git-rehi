cd3fc76d4243083a2ec1f704958be6abd85a3084
Max Kirillov <max@max630.net>
2016-02-08 00:20:41 +0200

    IMPL: add Monad.Catch instances

--- app/MonadStateIO.hs
+++ app/MonadStateIO.hs
@@ -6,6 +6,7 @@ module MonadStateIO (evalStateT,execStateT) where
 import Data.Tuple (swap)
 
 import qualified Control.Applicative as App
+import qualified Control.Monad.Catch as MC
 import qualified Control.Monad.Trans as MT
 import qualified Control.Monad.Trans.State as MTS
 import qualified Control.Monad.Trans.Reader as MTR
@@ -26,10 +27,30 @@ instance MIO.MonadIO m => MS.MonadState s (App.WrappedMonad (MTR.ReaderT (RefTag
   put val = App.WrapMonad (MR.ask >>= (MIO.liftIO . (`R.writeIORef` val) . getRef))
   state func = App.WrapMonad (MR.ask >>= (MIO.liftIO . (\ref -> R.atomicModifyIORef ref (swap . func)) . getRef))
 
-instance (MIO.MonadIO m, MR.MonadReader r m) => MR.MonadReader r (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) where
+instance MR.MonadReader r m => MR.MonadReader r (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) where
   ask = App.WrapMonad (MT.lift MR.ask)
   reader f = App.WrapMonad (MT.lift $ MR.reader f)
   local f (App.WrapMonad (MTR.ReaderT sub)) = App.WrapMonad (MTR.ReaderT (\v -> MR.local f (sub v)))
+
+instance MC.MonadThrow m => MC.MonadThrow (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) where
+  throwM ex = App.WrapMonad (MC.throwM ex)
+
+instance MC.MonadCatch m => MC.MonadCatch (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) where
+  catch (App.WrapMonad body) handler = App.WrapMonad impl
+   where
+    impl = MC.catch
+              body
+              (App.unwrapMonad . handler)
+
+instance MC.MonadMask m => MC.MonadMask (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) where
+  mask mkBody0 = App.WrapMonad (MC.mask (\rest -> App.unwrapMonad (mkBody0 $ w rest)))
+    where
+      w :: ((MTR.ReaderT (RefTag s) m) a -> (MTR.ReaderT (RefTag s) m) a) -> (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) a -> (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) a
+      w rest = App.WrapMonad . rest . App.unwrapMonad
+  uninterruptibleMask mkBody0 = App.WrapMonad (MC.uninterruptibleMask (\rest -> App.unwrapMonad (mkBody0 $ w rest)))
+    where
+      w :: ((MTR.ReaderT (RefTag s) m) a -> (MTR.ReaderT (RefTag s) m) a) -> (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) a -> (App.WrappedMonad (MTR.ReaderT (RefTag s) m)) a
+      w rest = App.WrapMonad . rest . App.unwrapMonad
 
 runIOStateT :: MIO.MonadIO m => IOStateT s m a -> s -> m (R.IORef s, a)
 runIOStateT (App.WrapMonad func) initS = do
