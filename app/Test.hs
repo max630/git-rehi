246cec3690a0efd1038dc54857501fccc87046f1
Max Kirillov <max@max630.net>
2016-02-12 01:05:23 +0200

    more unittests

--- app/Test.hs
+++ app/Test.hs
@@ -7,7 +7,7 @@ import Test.HUnit (test,(~:),(~=?),(~?=),(@=?),(@?=),(@?),runTestTT)
 
 import Prelude hiding (putStrLn,putStr,writeFile,readFile)
 
-import Control.Monad.Catch(finally)
+import Control.Monad.Catch(finally,catch)
 import Control.Monad.State(execState)
 import Data.ByteString.Builder (toLazyByteString, word64HexFixed, string7)
 import Data.ByteString.Lazy (toStrict)
@@ -24,6 +24,15 @@ main = undefined
 
 allTests = test [ "regex" ~:
                     [ "split keeps last " ~: regex_split "a b c" " " ~?= ["a", "b", "c"] ]
+                , "find_sequence" ~:
+                    [ "linear" ~: test_findseq [(1,[2]),(2,[3]),(3,[4])] 4 1 [] ~?= [h 3, h 2, h 1]
+                    , "diamond" ~: test_findseq [(4,[2,3]),(2,[1]),(3,[1]),(1,[0])] 0 4 [] ~?= map h [1,3,2,4]
+                    , "simple_branch" ~: test_findseq [(1,[2,3]),(2,[3,5])] 2 1 [] ~?= [h 1]
+                    , "shortest" ~: test_findseq [(1,[2,3]),(2,[6]),(3,[4]),(4,[6]),(6,[7,10])] 6 1 [] ~?= map h [2,1]
+                    , "through" ~: test_findseq [(1,[2,3]),(2,[6]),(3,[4]),(4,[6]),(6,[7,10])] 6 1 [4] ~?= map h [4,3,1]
+                    {- TODO: learn assert for failure , "parallel_throughs" ~: test_findseq [(1,[2,3]),(2,[6]),(3,[4]),(4,[6]),(6,[7,10])] 6 1 [2,4] ~?= map h [] -}
+                    , "inner merge" ~: test_findseq [(1,[2,5]),(2,[3]),(3,[4]),(4,[]),(5,[6]),(6,[3,7]),(7,[4])] 4 1 [] ~?= map h [3,6,5,2,1]
+                    , "inner merge through" ~: test_findseq [(1,[2,5]),(2,[3]),(3,[4]),(4,[]),(5,[6]),(6,[3,7]),(7,[4])] 4 1 [7] ~?= map h [7,6,5,1] ]
                 , "build_rebase_sequence" ~:
                     [ "1" ~: brs1 ~?= [Pick (hashes !! 0)]
                     , "2" ~: brs2 ~?= []
@@ -33,8 +42,13 @@ allTests = test [ "regex" ~:
                                       , Mark "tmp_2"
                                       , Reset "@tmp_1"
                                       , p 2
-                                      , Merge (Just (hashes !! 4)) ["HEAD", "@tmp_2"] False False] ]
-                , "parse_cli" ~: p1 ~?= Run "origin/b4" Nothing [] (Just "origin/base") Nothing True
+                                      , Merge (Just (hashes !! 4)) ["HEAD", "@tmp_2"] False False]
+                    , "inner_merge_after_merge " ~: test_brs [(1,[2,3]),(2,[4]),(3,[4]),(4,[5,6]),(5,[7]),(6,[8])] 5 1 []
+                                                      ~?= [ Merge (Just (hashes !! 4)) ["HEAD",hashes !! 6] False False
+                                                          , Mark "tmp_1", p 3, Mark "tmp_2", Reset "@tmp_1", p 2
+                                                          , Merge (Just (hashes !! 1)) ["HEAD","@tmp_2"] False False ] ]
+                , "parse_cli_p1" ~: p1 ~?= Run "origin/b4" Nothing [] (Just "origin/base") Nothing True
+                , "parse_cli" ~: test_parse_cli
                 , "parse_todo" ~:
                     [ "1" ~: tp1 >>= (@?= [Merge (Just "f1") ["HEAD", "f2"] False False])
                     , "2" ~: tp2 >>= (@?= [Edit "316bf9c"])
@@ -50,6 +64,7 @@ allTests = test [ "regex" ~:
                     ]
   where
     p n = Pick (hashes !! n)
+    h n = Hash (hashes !! n)
 
 hashes = map (B.reverse . toStrict . toLazyByteString . (string7 (replicate 24 '0') <>) . word64HexFixed) [0 ..]
 
@@ -63,6 +78,13 @@ noCommits = Commits Sync M.empty M.empty M.empty
 test_brs commits from to throughs =
   build_rebase_sequence
     (Commits Sync M.empty M.empty (M.fromList $ map i2c commits))
+    (Hash (hashes !! from))
+    (Hash (hashes !! to))
+    (map (Hash . (hashes !!)) throughs)
+
+test_findseq commits from to throughs =
+  find_sequence
+    (M.fromList $ map i2c commits)
     (Hash (hashes !! from))
     (Hash (hashes !! to))
     (map (Hash . (hashes !!)) throughs)
@@ -110,3 +132,16 @@ pl1 = execState
                                 <> "97277bafae875c930ea7c4a338a82073c897f7f0 76dee8a19ec9fddea0a02d99b0d1e00b1ef1caba:"
                                 <> "Merge remote-tracking branch 'origin/b2'\n\nresolve conflict also\n"))
         noCommits
+
+test_parse_cli =
+  [ parse_cli ["a"] ~?= Run "a" Nothing [] Nothing Nothing False
+  , parse_cli ["a","c"] ~?= Run "a" Nothing [] Nothing (Just "c") False
+  , parse_cli ["a","b..d","c"] ~?= Run "a" (Just "b") [] (Just "d") (Just "c") False
+  , parse_cli ["a","b..","c"] ~?= Run "a" (Just "b") [] Nothing (Just "c") False
+  , parse_cli ["a","..d","c"] ~?= Run "a" Nothing [] (Just "d") (Just "c") False
+  , parse_cli ["a","b..e..d","c"] ~?= Run "a" (Just "b") ["e"] (Just "d") (Just "c") False
+  , parse_cli ["a","..e..","c"] ~?= Run "a" Nothing ["e"] Nothing (Just "c") False
+  , parse_cli ["a","..e.."] ~?= Run "a" Nothing ["e"] Nothing Nothing False
+  , parse_cli ["a","b..e..f..d","c"] ~?= Run "a" (Just "b") ["e","f"] (Just "d") (Just "c") False
+  {- TODO: errors -}
+  ]
