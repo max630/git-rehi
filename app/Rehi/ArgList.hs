e76021f71204365bcefed7e4e229ba96a0184673
Max Kirillov <max@max630.net>
2016-08-21 22:44:39 +0300

    Rehi.ArgList modulem to represent commandline arguments
    Allows safely and specify commandline, adding arguments when necessary.
    
    * string _literal_ is split by spaces
    * list literal is as is

---
+++ app/Rehi/ArgList.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+module Rehi.ArgList where
+
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (split)
+import Data.Monoid (Monoid)
+
+import GHC.Exts(IsString, fromString, IsList, Item, fromList, toList)
+
+newtype ArgList = ArgList { getArgList :: [ByteString] } deriving (Show,Monoid)
+
+instance IsString ArgList where
+  fromString = ArgList . split ' ' . fromString
+
+instance IsList ArgList where
+  type Item ArgList = ByteString
+  fromList = ArgList
+  toList = getArgList
